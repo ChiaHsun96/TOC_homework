e1d6b827efa94af85f475c0546a476723812e8a5
f74072162 <f74072162@gs.ncku.edu.tw>
2020-12-27 21:07:08 +0800

    ngrok

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from test import *
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageCarouselTemplate,ImageCarouselColumn,DatetimePickerAction,FlexSendMessage,ImageSendMessage
@@ -99,12 +100,12 @@ def fsm_pic(reply_token,userid):
 def crawl(userid ,county):
     line_bot_api = LineBotApi(channel_access_token)
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless") 
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    options = Options()
+    options.add_argument("--disable-notifications")
+ 
+    chrome = webdriver.Chrome('./chromedriver', chrome_options=options)
+
 
     chrome.get("https://www.cwb.gov.tw/V8/C/")
 
